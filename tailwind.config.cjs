e99e5f59af0539dedb2c8a0be1a5f097f605fae8
joar <joar.sabel@netikka.fi>
2024-02-15 15:22:51 +0200

    Add basic Nord color scheme and begin conversion

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 export default {
   content: ["./src/**/*.{html,js,ts,astro}"],
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',  
+      'nord0': '#2E3440',
+      'nord1': '#384252',
+      'nord2': '#434C5E',
+      'nord3': '#4C566A',
+      'nord4': '#D8DEE9',
+      'nord5': '#E5E9F0',
+      'nord6': '#ECEFF4',
+      'nord7': '#8FBCBB',
+      'nord8': '#88C0D0',
+      'nord9': '#81A1C1',
+      'nord10': '#5E81AC',
+      'nord11': '#BF616A',
+      'nord12': '#D0B770',
+      'nord13': '#EBCB8B',
+      'nord14': '#A3BE8C',
+      'nord15': '#B48EAD',
+    },
+    },
   },
   plugins: [
    require('@tailwindcss/typography'),
