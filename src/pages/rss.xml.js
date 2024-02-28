b8485db2348826fcf9fe493c4bc30a0ac7fed17a
joar <joar.sabel@netikka.fi>
2024-02-28 20:58:56 +0200

    Mega conversion to content collections
    Converted blog/ and tags/ to use content collections which required a
    little bit of work here and there.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
   return rss({
     title: 'joar.a.developer',
     description: 'A collection of random thoughts regarding various things',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./posts/**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
