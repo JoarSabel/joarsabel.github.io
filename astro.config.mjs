a63d2b565f4d9b17c377029ce72a62abf90c198b
joar <joar.sabel@netikka.fi>
2024-02-28 22:17:02 +0200

    Maybe make tailwind styles work?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://joarsabel.github.io",
-  base: '/joarsabel.github.io',
+  base: '/',
 });
